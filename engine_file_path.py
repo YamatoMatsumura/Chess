f8c9e5cb8a580b5482fc33bca8030be66dda407a
Yamato Matsumura <yamtheyam104@gmail.com>
2024-01-04 22:44:00 -0600

    Resturctured project, updated gitignore

--- engine_file_path.py
+++
@@ -1,2 +0,0 @@
-def get_file_path():
-    return "C:\\Users\\yamth\\OneDrive\\Desktop\\CS50_Final_Project\\stockfish\\stockfish-windows-x86-64-modern.exe"
---
+++ src/engine_file_path.py
@@ -0,0 +1,2 @@
+def get_file_path():
+    return "C:\\Users\\yamth\\OneDrive\\Desktop\\Github_Projects\\chess\\stockfish\\stockfish-windows-x86-64-modern.exe"
